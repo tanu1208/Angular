3745ad512d4288a4a6212f741d8cea82e5b966d4
Tanu <tanu-95@hotmail.com>
2020-10-05 14:26:18 +0200

    initialized card, list and nav-bar

---
+++ Property-app/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  //template: `<h1>Hi I am a card</h1>`,
+  templateUrl: 'property-card.component.html',
+  //styles: ['h1 {font-weight: normal;}']
+  styleUrls: ['property-card.component.css']
+})
+
+export class PropertyCardComponent {
+
+}
