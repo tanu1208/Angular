f806294ae62206b1bb816768a14bcab24e9d8317
Tanu <tanu-95@hotmail.com>
2020-10-06 18:24:15 +0200

    added HTTP with pipe

--- Property-app/Frontend/src/app/property/property-card/property-card.component.ts
+++ Property-app/Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-card',
@@ -9,12 +10,5 @@ import { Component } from '@angular/core';
 })
 
 export class PropertyCardComponent {
-
-  Property: any = {
-    "Id":1,
-    "Name":"KA20",
-    "Type":"House",
-    "Price":12000
-  }
-
+  @Input() property : IProperty
 }
--- Property-app/Frontend/src/app/property/property-list/property-list.component.ts
+++ Property-app/Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
+
 
 @Component({
   selector: 'app-property-list',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+  properties: Array<IProperty>;
+
+  constructor(private housingService: HousingService) { }
 
   ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+        this.properties = data;
+        console.log(data);
+      }, error => {
+        console.log('HttpError: ');
+        console.log(error);
+      }
+    )
   }
-
 }
