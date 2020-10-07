1538596e08dc75d4c12f8703f9fb0f512308c588
Tanu <tanu-95@hotmail.com>
2020-10-07 16:51:26 +0200

    added two way binding and list property page + a preview of the current listed property

--- Property-app/Frontend/src/app/app.module.ts
+++ Property-app/Frontend/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -46,8 +49,11 @@ const appRoutes: Routes = [
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    ButtonsModule.forRoot(),
+    TabsModule.forRoot(),
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
   providers: [HousingService, UserService, AlertifyService, AuthService],
--- Property-app/Frontend/src/app/property/add-property/add-property.component.ts
+++ Property-app/Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-add-property',
@@ -9,10 +11,28 @@ import { NgForm } from '@angular/forms';
 })
 export class AddPropertyComponent implements OnInit {
   @ViewChild('Form') addPropertyForm: NgForm;
-  SellRent = '1';
+  @ViewChild('formTabs') formTabs: TabsetComponent;
+  
+  propertyTypes: Array<string> = ['House', 'Apartment', "Duplex"]
+  furnishTypes: Array<string> = ['Fully', 'Semi', "Unfurnished"]
+
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: null
+  };
+  
   constructor(private router: Router) { }
 
   ngOnInit() {
+    // this.addPropertyForm.controls['Name'].setValue('Default Value');
+
+    setTimeout(() => {
+      this.addPropertyForm.controls['Name'].setValue('');
+    });
+
   }
 
   onBack() {
@@ -22,6 +42,10 @@ export class AddPropertyComponent implements OnInit {
   onSubmit() {
     console.log('Congrats, form Submitted');
     console.log(this.addPropertyForm);
+  }
+
+  selectTab(tabId: number) {
+    this.formTabs.tabs[tabId].active = true;
   }
 
 }
